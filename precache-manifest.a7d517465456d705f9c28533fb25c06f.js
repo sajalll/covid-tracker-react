795cc2762962e6c924c1ecf32059eab4330bf22d
SAJAL <wrk.sajal@gmail.com>
2022-01-20 17:37:35 +0530

    Updates

---
+++ precache-manifest.a7d517465456d705f9c28533fb25c06f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cf5193a584dd85369e0034d681ebb1",
+    "url": "/covid-tracker-react/index.html"
+  },
+  {
+    "revision": "71835e8703448b526917",
+    "url": "/covid-tracker-react/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "af1ec1bf0a9e95f9d83b",
+    "url": "/covid-tracker-react/static/css/main.0dab1bb6.chunk.css"
+  },
+  {
+    "revision": "71835e8703448b526917",
+    "url": "/covid-tracker-react/static/js/2.f29f8e61.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker-react/static/js/2.f29f8e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1ec1bf0a9e95f9d83b",
+    "url": "/covid-tracker-react/static/js/main.f41fe908.chunk.js"
+  },
+  {
+    "revision": "ee1a0f2388b362f767d2",
+    "url": "/covid-tracker-react/static/js/runtime-main.0ee3126c.js"
+  }
+]);
